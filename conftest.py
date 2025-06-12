72150a6de3b68636e0deafe1c936e64bfd1e34bb
Vlada Maksimova <vladamaximova2003@gmail.com>
2025-06-13 03:56:37 +0500

    Первый тест на основе Page Object

--- conftest.py
+++ conftest.py
@@ -1,11 +1,11 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
 
 def pytest_addoption(parser):
-    parser.addoption("--language", action="store", default="en")
-
+    parser.addoption("--language", action="store", default="en", help="Choose language")
 
 @pytest.fixture(scope="function")
 def browser(request):
@@ -13,7 +13,8 @@ def browser(request):
     options = Options()
     options.add_experimental_option("prefs", {"intl.accept_languages": user_language})
     print(f"Запуск браузера с установленным языком: {user_language}")
-    browser = webdriver.Chrome(options=options)
+    service = Service(ChromeDriverManager().install())
+    browser = webdriver.Chrome(service=service, options=options)
     yield browser
     print("\nЗакрытие браузера...")
     browser.quit()
