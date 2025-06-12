3f4bc047b2d59c32762fff621c52ef9ecbf140a1
Vlada Maksimova <vladamaximova2003@gmail.com>
2025-06-13 04:11:38 +0500

    Адекватное сообщение об ошибке

--- conftest.py
+++ conftest.py
@@ -4,8 +4,10 @@ from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 
+
 def pytest_addoption(parser):
     parser.addoption("--language", action="store", default="en", help="Choose language")
+
 
 @pytest.fixture(scope="function")
 def browser(request):
